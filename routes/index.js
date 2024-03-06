45399daa3b34498a82c4377e44a5c07ca573a72f
Oscar Gomez Diez <oscarfotodiez@gmail.com>
2024-03-06 19:13:57 +0100

    implementing is authenticated

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
+const { isAuthenticated } = require("../middleware/jwt.middleware.js")
+
 module.exports = app => {
 
     // TODO: incluir isAuthenticated en todas las routes
     //const { isAuthenticated } = require("./middleware/jwt.middleware")
 
     const clientRouter = require("./client.routes.js")
-    app.use("/api/clients", clientRouter)
+    app.use("/api/clients", isAuthenticated, clientRouter)
 
     const professionalRouter = require("./professional.routes.js")
-    app.use("/api/professionals", professionalRouter)
+    app.use("/api/professionals", isAuthenticated, professionalRouter)
 
     const petRouter = require("./pet.routes.js")
-    app.use("/api/pets", petRouter)
+    app.use("/api/pets", isAuthenticated, petRouter)
 
     const requestRouter = require("./request.routes.js")
-    app.use("/api/requests", requestRouter)
+    app.use("/api/requests", isAuthenticated, requestRouter)
 
     const authRoutes = require("./auth.routes.js")
     app.use("/api/auth", authRoutes)
