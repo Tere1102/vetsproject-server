afac1d147015ca3ed78b48df105a412934e66222
Patri Lago <patrilagoespino@gmail.com>
2024-03-07 13:34:30 +0100

    crash fixed

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 
-const uploader = require('./../middleware/cloudinary.middleware')
+const uploader = require('./../middleware/upload.middleware')
 
 router.post('/image', uploader.single('imageData'), (req, res) => {
 
